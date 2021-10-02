15655ae4b6b72213c93daac4ad9f160c959bb268
felipexd3 <31152127fb>
2021-10-02 20:27:40 -0300

    Criando estrutura e testes para o job

--- domain/job.go
+++ domain/job.go
@@ -1,13 +1,52 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type Job struct {
-	Id               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	Id               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	VideoID          string    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
+
+func NewJob(output string, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+
+	job.prepare()
+
+	err := job.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (job *Job) prepare() {
+	job.Id = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+}
+
+func (job *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(job)
+	if err != nil {
+		return err
+	}
+	return nil
 }
